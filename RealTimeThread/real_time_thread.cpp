9b74dc9a8416e4a0e4906688ebe7a392119bd555
Harel Grecht <Harel.Grecht@gmail.com>
2025-02-26 18:40:59 +0200

    [FIX] Fix creating instance of the thread and fix access to the logger

--- RealTimeThread/real_time_thread.cpp
+++ RealTimeThread/real_time_thread.cpp
@@ -1,15 +1,16 @@
 #include "real_time_thread.hpp"
 
-RealTimeThread::RealTimeThread(void (*routine)(void *), void *arg) : thread_(routine) {
+RealTimeThread::RealTimeThread(void (*routine)(void *), void *arg) {
+    thread_ = std::thread(routine);
     pthread_t nativeHandle = thread_.native_handle();
 
     sched_param schedParam;
     schedParam.sched_priority = MAX_PRIORETY;
     int ret = pthread_setschedparam(nativeHandle, THREAD_POLICY, &schedParam);
     if (ret != 0) {
-        Logger.getInstance().Log(LogLevel.ERROR, "Failed to set scheduling parameters: " << strerror(ret));
+        Logger::getInstance().Log(LogLevel::ERROR, "Failed to set scheduling parameters: " + std::string(strerror(ret)));
     } else {
-        Logger.getInstance().Log(LogLevel.INFO, "Thread scheduling parameters set successfully.");
+        Logger::getInstance().Log(LogLevel::INFO, "Thread scheduling parameters set successfully.");
     }
 }
 
