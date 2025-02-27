2b9b09df5e37dab30a66555bc6e9e2feb2efe04f
Harel Grecht <Harel.Grecht@gmail.com>
2025-02-27 09:45:04 +0200

    [FIX] Fix thread instance, some functions calls, variable typos, json calls

--- packetRouter/packet_router.hpp
+++ packetRouter/packet_router.hpp
@@ -21,22 +21,27 @@ struct PacketInfo {
 template <typename T>
 class PacketRouter {
     public:
-        PacketRouter();
+        PacketRouter(ThreadSafeQueue<T>& receiveQueue, ThreadSafeQueue<T>& processQueue, ThreadSafeQueue<T>& fromTunnelQueue, ThreadSafeQueue<T> guiQueue);
         
-        void packetRouteHandler(ThreadSafeQueue<T>& receiveQueue, ThreadSafeQueue<T>& processQueue, ThreadSafeQueue<T>& fromTunnelQueue);
+        void packetRouteHandler();
 
     private:
-        int IPV4_HEADER_SIZE;
-        int PACKET_HEADER_SIZE;
-        int MIN_PACKET_SIZE;
-        int SRC_IP_OFFSET;
-        int DEST_IP_OFFSET;
-        int SRC_PORT_OFFSET;
-        int DEST_PORT_OFFSET;
-        int PAYLOAD_OFFSET;
-        uint16_t GUI_PAYLOAD_ICD;
+        int IPV4_HEADER_SIZE_;
+        int PACKET_HEADER_SIZE_;
+        int MIN_PACKET_SIZE_;
+        int SRC_IP_OFFSET_;
+        int DEST_IP_OFFSET_;
+        int SRC_PORT_OFFSET_;
+        int DEST_PORT_OFFSET_;
+        int PAYLOAD_OFFSET_;
+        uint16_t GUI_PAYLOAD_ICD_;
 
-        EthDevice tunnelDevice; // Member variable to store the tunnel device
+        EthDevice tunnelDevice_; // Member variable to store the tunnel device
+        ThreadSafeQueue<T> processQueue;
+        ThreadSafeQueue<T> receiveQueue;
+        ThreadSafeQueue<T> fromTunnelQueue;
+        ThreadSafeQueue<T> guiQueue;
+
 
         bool isFromTunnel(const std::string& srcIP, const std::string& destIP);
         bool isFromGui(PacketInfo& udpPkt);
@@ -44,34 +49,34 @@ class PacketRouter {
 };
 
 template <typename T>
-PacketRouter<T>::PacketRouter() {
+PacketRouter<T>::PacketRouter(ThreadSafeQueue<T>& receiveQueue, ThreadSafeQueue<T>& processQueue, ThreadSafeQueue<T>& fromTunnelQueue, ThreadSafeQueue<T> guiQueue) 
+    : receiveQueue(receiveQueue), processQueue(processQueue), fromTunnelQueue(fromTunnelQueue) {
     std::ifstream routeConfig("route_settings.json");
     if (!routeConfig.is_open()) {
-        throw std::runtime_error("Failed to open user settings file: " + userSettingsFile);
+        throw std::runtime_error("Failed to open user settings file: " + routeConfig);
     }
     nlohmann::json jsonConfig;
     jsonConfig >> routeConfig;
 
     // Extract values from the JSON object
-        IPV4_HEADER_SIZE = jsonConfig.at("IPV4_HEADER_SIZE").get<int>();
-        PACKET_HEADER_SIZE = jsonConfig.at("PACKET_HEADER_SIZE").get<int>();
-        MIN_PACKET_SIZE = jsonConfig.at("MIN_PACKET_SIZE").get<int>();
-        SRC_IP_OFFSET = jsonConfig.at("SRC_IP_OFFSET").get<int>();
-        DEST_IP_OFFSET = jsonConfig.at("DEST_IP_OFFSET").get<int>();
-        SRC_PORT_OFFSET = jsonConfig.at("SRC_PORT_OFFSET").get<int>();
-        DEST_PORT_OFFSET = jsonConfig.at("DEST_PORT_OFFSET").get<int>();
-        PAYLOAD_OFFSET = jsonConfig.at("PAYLOAD_OFFSET").get<int>();
-        GUI_PAYLOAD_ICD = jsonConfig.at("guiPayloadICD").get<uint16_t>();
+        IPV4_HEADER_SIZE_ = jsonConfig.at("IPV4_HEADER_SIZE").get<int>();
+        PACKET_HEADER_SIZE_ = jsonConfig.at("PACKET_HEADER_SIZE").get<int>();
+        MIN_PACKET_SIZE_ = jsonConfig.at("MIN_PACKET_SIZE").get<int>();
+        SRC_IP_OFFSET_ = jsonConfig.at("SRC_IP_OFFSET").get<int>();
+        DEST_IP_OFFSET_ = jsonConfig.at("DEST_IP_OFFSET").get<int>();
+        SRC_PORT_OFFSET_ = jsonConfig.at("SRC_PORT_OFFSET").get<int>();
+        DEST_PORT_OFFSET_ = jsonConfig.at("DEST_PORT_OFFSET").get<int>();
+        PAYLOAD_OFFSET_ = jsonConfig.at("PAYLOAD_OFFSET").get<int>();
+        GUI_PAYLOAD_ICD_ = jsonConfig.at("guiPayloadICD").get<uint16_t>();
 
-        tunnelDevice = NetworkManager::getDeviceByRole(EthDevice::Role::TUNNEL);
+        tunnelDevice_ = NetworkManager::getDeviceByRole(EthDevice::Role::TUNNEL);
 }
 
 template <typename T>
-void PacketRouter<T>::packetRouteHandler(ThreadSafeQueue<T>& receiveQueue, ThreadSafeQueue<T>& toSendQueue, ThreadSafeQueue<T>& fromTunnelQueue) {
-    T rawPacket;
-    while(true) {
+void PacketRouter<T>::packetRouteHandler() {
+    T rawPacket;    while(true) {
         T rawPacket = receiveQueue.dequeue();
-        PacketInfo packet = parsePacket(rawPacket, udpPkt)
+        PacketInfo packet = parsePacket(rawPacket);
         if(!packet) continue;
         
         if (isFromTunnel(packet.srcIP, packet.destIP)) { 
@@ -83,17 +88,17 @@ void PacketRouter<T>::packetRouteHandler(ThreadSafeQueue<T>& receiveQueue, Threa
             toSendQueue.enqueue(rawPacket);
         }   
     } 
-    std::this_thread::sleep_for(THREAD_SLEEP_DURATION);
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
 }
 
 template <typename T>
 bool PacketRouter<T>::isFromTunnel(const std::string& srcIP, const std::string& destIP) {
-    return (srcIP == tunnelDevice.getDestIp()) && (destIP == tunnelDevice.getSelfIp());
+    return (srcIP == tunnelDevice_.getDestIp()) && (destIP == tunnelDevice_.getSelfIp());
 }
 
 template <typename T>
 bool PacketRouter<T>::isFromGui(PacketInfo& packet) {
-    return (packet.payload[0] == GUI_PAYLOAD_ICD) && (packet.payload[packet.payload.size() - 2] == GUI_PAYLOAD_ICD);
+    return (packet.payload[0] == GUI_PAYLOAD_ICD_) && (packet.payload[packet.payload.size() - 2] == GUI_PAYLOAD_ICD_);
 }
 
 template <typename T>
@@ -101,16 +106,16 @@ PacketInfo PacketRouter<T>::parsePacket(const T& packet) {
     PacketInfo packet;
     char ipStr[INET_ADDRSTRLEN];
 
-    uint32_t srcIp = ntohl(*((uint32_t*)(packet.data() + SRC_IP_OFFSET)));
+    uint32_t srcIp = ntohl(*((uint32_t*)(packet.data() + SRC_IP_OFFSET_)));
     inet_ntop(AF_INET, &srcIp, ipStr, sizeof(ipStr));
     packet.srcIP = std::string(ipStr);
 
-    uint32_t destIp = ntohl(*((uint32_t*)(packet.data() + DEST_IP_OFFSET)));
+    uint32_t destIp = ntohl(*((uint32_t*)(packet.data() + DEST_IP_OFFSET_)));
     inet_ntop(AF_INET, &destIp, ipStr, sizeof(ipStr));
     packet.destIP = std::string(ipStr);
 
-    if (packet.size() > UDP_PAYLOAD_OFFSET) {
-        packet.payload.assign(packet.begin() + PAYLOAD_OFFSET, packet.end());
+    if (packet.size() > PAYLOAD_OFFSET_) {
+        packet.payload.assign(packet.begin() + PAYLOAD_OFFSET_, packet.end());
     }
 
     return packet;
