bd602a048a88ac7124c5ecb4c03960eef6cca550
Harel Grecht <Harel.Grecht@gmail.com>
2025-02-27 08:26:00 +0200

    [ADD] Add tunnelReciverProcess constructor

--- tunnel_receiver_process/tunnel_reciver_process.hpp
+++ tunnel_receiver_process/tunnel_reciver_process.hpp
@@ -4,19 +4,23 @@
 #include <vector>
 #include "../ThreadSafeQueue/threadSafeQueue.hpp"
 
-constexpr size_t HEADER_SIZE = 8;
+constexpr size_t RECEIVER_HEAER_SIZE = 8;
 
 template <typename T>
 class TunnelReceiverProcess {
     public:
-        void tunnelReceiveHandler();
+
+        TunnelReceiverProcess(threadSafeQueue<T>& sendQueue, threadSafeQueue<T>& fromTunnelQueue);
 
     private:
         T rawPacketBuffer_;
         T payloadBuffer_;
+        
+        threadSafeQueue<T> sendQueue;
+        threadSafeQueue<T> fromTunnelQueue;
 
-        void getPayload(threadSafeQueue<T>& fromTunnelQueue);
-        void mainProcess(threadSafeQueue<T>& sendQueue);
+        void getPayload();
+        void mainProcess();
 
 };
 
@@ -24,21 +28,24 @@ class TunnelReceiverProcess {
     Note:
         You can add here processing of the must icd as known "*#M{info}"
 */
+
 template <typename T>
-void TunnelReceiverProcess<T>::getPayload(threadSafeQueue<T>& fromTunnelQueue) {
+TunnelReceiverProcess<T>::TunnelReceiverProcess(threadSafeQueue<T>& sendQueue, threadSafeQueue<T>& fromTunnelQueue) 
+    : sendQueue(sendQueue), fromTunnelQueue(fromTunnelQueue) { }
+template <typename T>
+void TunnelReceiverProcess<T>::getPayload() {
     rawPacketBuffer_ = fromTunnelQueue.dequeue();
-    if (rawPacketBuffer.size() > HEADER_SIZE) {
-        payloadBuffer.assign(rawPacketBuffer.begin() + HEADER_SIZE, rawPacketBuffer.end());
+    if (rawPacketBuffer.size() > RECEIVER_HEAER_SIZE) {
+        payloadBuffer.assign(rawPacketBuffer.begin() + RECEIVER_HEAER_SIZE, rawPacketBuffer.end());
     }
 
 }
 
 template <typename T>
-void TunnelReceiverProcess<T>::mainProcess(threadSafeQueue<T>& sendQueue) {
+void TunnelReceiverProcess<T>::mainProcess() {
     /*
         Perform any necessary transformations on payloadBuffer_, then pass to the next queue.
         Data transformation or manipulation code goes here...
     */
    sendQueue.enqueue(payloadBuffer_);
 }
-
